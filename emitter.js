d157120f32ec3b5038400d07b03fe0902f71b0e3
Kuznecoff <artem98kuznecov@mail.ru>
2018-11-06 11:35:02 +0500

    первая попытка

--- emitter.js
+++ emitter.js
@@ -12,32 +12,71 @@ const isStar = true;
  */
 function getEmitter() {
     return {
+        events: new Map(),
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!this.events.has(event)) {
+                this.events.set(event, []);
+            }
+            handler = typeof handler === 'object' ? handler
+                : { func: handler, times: Infinity, frequency: 1, count: 0 };
+            this.events.get(event).push({ context, handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            this.events.forEach((v, k) => {
+                if (k.indexOf(event) === 0) {
+                    let eventsToOff = v.filter((e) => e.context === context);
+                    for (let i = 0; i < eventsToOff.length; i++) {
+                        let current = this.events.get(k);
+                        current.splice(current.indexOf(eventsToOff[i]), 1);
+                    }
+                }
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            let ev;
+            let toEmit = event.split('.');
+            for (let i = 0; i < toEmit.length; i++) {
+                let currentEvent = toEmit.slice(0, toEmit.length - i).join('.');
+                if (this.events.has(currentEvent)) {
+                    ev = this.events.get(currentEvent);
+                }
+                if (ev) {
+                    ev.forEach(e => {
+                        if (e.handler.times !== 0 && e.handler.count % e.handler.frequency === 0) {
+                            e.handler.func.call(e.context);
+                            e.handler.times--;
+                        }
+                        e.handler.count++;
+                    });
+                }
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +86,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            times = times <= 0 ? Infinity : times;
+            this.on(event, context, { func: handler, times, frequency: 1, count: 0 });
+
+            return this;
         },
 
         /**
@@ -59,9 +102,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            frequency = frequency <= 0 ? 1 : frequency;
+            this.on(event, context, { func: handler, times: Infinity, frequency, count: 0 });
+
+            return this;
         }
     };
 }
